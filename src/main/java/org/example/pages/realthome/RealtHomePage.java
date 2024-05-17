f754286b82db214574e1febd1745c527608fc01b
Naza1re <mcarim@mail.ru>
2024-05-17 21:57:47 +0300

    fix :  closing alert that interrupt click to element

--- src/main/java/org/example/pages/realthome/RealtHomePage.java
+++ src/main/java/org/example/pages/realthome/RealtHomePage.java
@@ -10,13 +10,18 @@ public class RealtHomePage extends BasePage {
         super(driver);
     }
 
-    By countRooms = By.xpath("//*[@id=\"__next\"]/div[1]/div[1]/main/div/div/div/div[3]/div/div[2]/form/div/div[1]/div[2]/div/div");
+    By countRooms = By.xpath("//*[@id=\"__next\"]/div[1]/div[1]/main/div/div/div/div[4]/div/div[2]/form/div/div[1]/div[2]/div/div/span[1]");
     By optionTwoRooms = By.xpath("//*[contains(text(),'2к квартира')]");
-    By findButton = By.xpath("//*[@id=\"__next\"]/div[1]/div[1]/main/div/div/div/div[3]/div/div[2]/form/div/div[2]/div/a[2]");
-
+    By findButton = By.xpath("//*[@id=\"__next\"]/div[1]/div[1]/main/div/div/div/div[4]/div/div[2]/form/div/div[2]/div/a[2]");
+    By refuseAlert = By.xpath("//*[@id=\"gdpr-portal-root\"]/div/div/div/button[1]");
     public RealtHomePage enterCountRooms() {
         driver.findElement(countRooms).click();
         driver.findElement(optionTwoRooms).click();
+        return this;
+    }
+
+    public RealtHomePage closeAlert() {
+        driver.findElement(refuseAlert).click();
         return this;
     }
 
--- src/test/java/tests/searchapartments/SearchApartTest.java
+++ src/test/java/tests/searchapartments/SearchApartTest.java
@@ -5,18 +5,19 @@ import tests.base.BaseTest;
 
 public class SearchApartTest extends BaseTest {
 
-    /*@Test
+    @Test
     public void checkIsRedirectToListing() {
         basePage.open("https://realt.by");
 
         realtHomePage
+                .closeAlert()
                 .enterCountRooms()
                 .clickToFind();
         realtListingPage
                 .checkCountCards();
-    }*/
+    }
 
-    @Test
+    @Test()
     public void openMorningLesson(){
         basePage.open("https://newsdo.vsu.by");
         newSdoHomePage.enterCredentials()
